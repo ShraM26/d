8e8bc0da4968f8ddb3bc8bec2c1fe9f46d4c86ec
shram <shram2615@gmail.com>
2024-05-12 08:35:51 +0300

    4

--- js/task-4.js
+++ js/task-4.js
@@ -1,40 +1,33 @@
 'use strict'
 
-// const registerForm = document.querySelector('.login-form');
-
-// registerForm.addEventListener('submit', (event) => {
-//     event.preventDefault();
-    
-//     const { login, password } = event.target.elements;
-//     const email = login.value;
-//     const userPassword = password.value;
-    
-//     if (email.trim() === "" || userPassword.trim() === "") {
-//         alert('Please fill in all the fields.');
-//         return;
-//     }
-
-//     console.log(`Email: ${email}, Password: ${userPassword}`);
-//     event.target.reset();
-// });
 const loginForm = document.querySelector('.login-form');
 
-loginForm.addEventListener('submit', (event) => {
+loginForm.addEventListener('submit', event => {
     event.preventDefault();
 
     const formData = {};
     const inputs = loginForm.elements;
 
     for (let input of inputs) {
-        if (input.type !== 'submit') {
-            formData[input.name] = input.value.trim();
-            if (!formData[input.name]) {
-                alert('All form fields must be filled in');
-                return;
-            }
+        if (input.type !== 'submit' && !input.value.trim()) {
+            alert('All form fields must be filled in');
+            return;
         }
+        formData[input.name] = input.value.trim();
+    }
+
+    if (!isValidEmail(formData.email)) {
+        alert('Please enter a valid email address');
+        return;
+    }
+
+    if (formData.password.length < 8) {
+        alert('Password must be at least 8 characters long');
+        return;
     }
 
     console.log(formData);
     loginForm.reset();
 });
+
+const isValidEmail = email => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
